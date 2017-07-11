af67b64f3d7c3856fe45981c4aa8ddc6b41934fe
drobin45 <drobin45@kent.edu>
2017-07-11 10:06:43 -0400

    Built Cat Class

---
+++ IntroToOOP/Cat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    class Cat
+    {
+        //Field (variable that belongs to a class) example
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        //Property example
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+
+        }
+
+        //Constructor (specialized method) example
+        public Cat()
+        {
+            //This is an example of a default constructor. A default constructor takes no arguments/parameters.
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("Is the cat hungry? " + isHungry);
+        }
+
+    }
+}
--- IntroToOOP/Program.cs
+++ IntroToOOP/Program.cs
@@ -10,7 +10,22 @@ namespace IntroToOOP
     {
         static void Main(string[] args)
         {
+            //When we initialize a new object, it is called Instantiating.
+            // An object needs to be instantiated before it can be used.
+            //All objects of a specific type are created fromt he same template - Class
+            //All objects from a class will have the same number and type of states, but...
+            //... each instance can have it's own unique values.
 
+            //The format we use to instantiate an object:
+            //ClassName objectName = new ClassName();
+            Cat mittens = new Cat();
+            //To create new object, we use a special method called a Constructor.
+            //Thinking about objects in real life, create three examples of objects. Your objects should have five states...
+            //... five behaviors. (In Notepad)
+
+
+
+            
 
 
         }
